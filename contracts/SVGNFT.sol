7713913ddd65868948d26828afb3c84850670139
Yoreee <rerebacchus@gmail.com>
2021-10-01 07:08:14 +0000

    fix svg

--- contracts/SVGNFT.sol
+++ contracts/SVGNFT.sol
@@ -1,59 +1,50 @@
-// contracts/MyNFT.sol
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.9;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "base64-sol/base64.sol";
 
-contract SVGNFT is ERC721URIStorage {
+contract SVGNFT is ERC721URIStorage, Ownable {
     uint256 public tokenCounter;
-    event createdSVGNFT(uint256 indexed tokenID, string tokenURI);
-    constructor() ERC721("SVGNFT", "svgNFt") {
+    event CreatedSVGNFT(uint256 indexed tokenId, string tokenURI);
+
+    constructor() ERC721("SVG NFT", "svgNFT")
+    {
         tokenCounter = 0;
     }
 
     function create(string memory svg) public {
         _safeMint(msg.sender, tokenCounter);
-        string memory imageURI = svgToImageUri(svg);
-        string memory tokenURI = formatTokenURI(imageURI);
-        _setTokenURI(tokenCounter, tokenURI);
-        //tokenuri?
-        //imageuri?
-        emit createdSVGNFT(tokenCounter, tokenURI);
-        
+        string memory imageURI = svgToImageURI(svg);
+        _setTokenURI(tokenCounter, formatTokenURI(imageURI));
         tokenCounter = tokenCounter + 1;
-        // return tokenURI;
+        emit CreatedSVGNFT(tokenCounter, svg);
     }
-
-    function svgToImageUri(string memory svg) public pure returns (string memory) {
-        // SVG <svg> </svg> 
-        // into this
-        // data: image/svg+xml;base64,<Base64-encoding>
-        // So we need to turn the SVG xml code string into a Base64 encoded string
-
-        string memory baseURL = "data: image/svg+xml;base64,";
+    // You could also just upload the raw SVG and have solildity convert it!
+    function svgToImageURI(string memory svg) public pure returns (string memory) {
+        // example:
+        // <svg width='500' height='500' viewBox='0 0 285 350' fill='none' xmlns='http://www.w3.org/2000/svg'><path fill='black' d='M150,0,L75,200,L225,200,Z'></path></svg>
+        // data:image/svg+xml;base64,PHN2ZyB3aWR0aD0nNTAwJyBoZWlnaHQ9JzUwMCcgdmlld0JveD0nMCAwIDI4NSAzNTAnIGZpbGw9J25vbmUnIHhtbG5zPSdodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2Zyc+PHBhdGggZmlsbD0nYmxhY2snIGQ9J00xNTAsMCxMNzUsMjAwLEwyMjUsMjAwLFonPjwvcGF0aD48L3N2Zz4=
+        string memory baseURL = "data:image/svg+xml;base64,";
         string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
-        string memory imageURI = string(abi.encodePacked(baseURL, svgBase64Encoded));
-
-        return imageURI;
+        return string(abi.encodePacked(baseURL,svgBase64Encoded));
     }
 
-    function formatTokenURI(string memory imageURI) public pure returns (string memory)
-    {
-        string memory baseURL = "data:application/json;base64,";
-        return string(abi.encodePacked(
-                baseURL,
-                Base64.encode(
-                    bytes(abi.encodePacked(
-                        '{"name": "SVG NFT",', 
-                        '"description: NFT made with SVG",', 
-                        '"attributes: "", image: "', 
-                            imageURI, 
-                        '"}'
+    function formatTokenURI(string memory imageURI) public pure returns (string memory) {
+        return string(
+                abi.encodePacked(
+                    "data:application/json;base64,",
+                    Base64.encode(
+                        bytes(
+                            abi.encodePacked(
+                                '{"name":"',
+                                "SVG NFT", // You can add whatever name here
+                                '", "description":"An NFT based on SVG!", "attributes":"", "image":"',imageURI,'"}'
+                            )
                         )
                     )
                 )
-        ));
+            );
     }
-
 }
